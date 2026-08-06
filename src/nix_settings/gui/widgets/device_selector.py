58333873f1ce2cbb9018af6eaad43347175bd7aa
madebycli <root.cli@proton.me>
2026-08-07 01:03:36 +0200

    fix: make audio selectors geometrically stable

--- src/nix_settings/gui/widgets/device_selector.py
+++ src/nix_settings/gui/widgets/device_selector.py
@@ -14,28 +14,37 @@ class DeviceSelector:
         selected_id: int | None,
         on_selected: Callable[[int], None],
         on_scroll: Callable[[Any], None] | None = None,
+        *,
+        width: int | None = None,
     ) -> None:
-        self._ids: list[int] = []
         self._selected_id = selected_id
         self._on_scroll = on_scroll
-        self.widget = Gtk.ComboBoxText()
+        self._model = Gtk.ListStore(str, int)
         active_index = 0
         for index, device in enumerate(devices):
-            self._ids.append(device.id)
-            self.widget.append_text(device.description)
+            self._model.append((device.description, device.id))
             if device.id == selected_id:
                 active_index = index
-        if self._ids:
-            self.widget.set_active(active_index)
+
+        self.widget = Gtk.ComboBox.new_with_model(self._model)
+        renderer = Gtk.CellRendererText()
+        renderer.set_property("ellipsize", 3)
+        renderer.set_property("ellipsize-set", True)
+        renderer.set_property("width-chars", 34)
+        self.widget.pack_start(renderer, True)
+        self.widget.add_attribute(renderer, "text", 0)
+        self.widget.set_active(active_index if devices else -1)
         self.widget.set_hexpand(True)
+        if width is not None:
+            self.widget.set_size_request(width, 34)
         self.widget.connect("changed", self._changed, on_selected)
         self.widget.connect("scroll-event", self._scroll)
 
     def _changed(self, combo: Any, callback: Callable[[int], None]) -> None:
-        index = int(combo.get_active())
-        if not 0 <= index < len(self._ids):
+        tree_iter = combo.get_active_iter()
+        if tree_iter is None:
             return
-        selected_id = self._ids[index]
+        selected_id = int(self._model[tree_iter][1])
         if selected_id == self._selected_id:
             return
         self._selected_id = selected_id
